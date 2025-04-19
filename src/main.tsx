71b3a4cdd0280ba4ab6b496347ad445f40ef3ec4
Nik <mrpepsi98@gmail.com>
2025-04-19 15:32:54 +0300

    решение проблемы перезагрузки страницы (деплой vercel)

--- src/main.tsx
+++ src/main.tsx
@@ -2,16 +2,16 @@ import './styles/index.css'
 import App from './App.tsx'
 import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter as Router } from 'react-router-dom'
 
 import { setupStore } from './store/store.ts'
 
 const store = setupStore()
 
 createRoot(document.getElementById('root')!).render(
-    <BrowserRouter>
+    <Router>
         <Provider store={store}>
             <App />
         </Provider>
-    </BrowserRouter>
+    </Router>
 )
